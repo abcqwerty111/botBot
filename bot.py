c92f4860d535fa0d2c2fafb2bbb6fb24814481e1
abcqwerty111 <47075954+abcqwerty111@users.noreply.github.com>
2020-04-01 10:59:58 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,14 +8,14 @@ bot = telebot.TeleBot('1046005810:AAG1-tPwvEb5nwEpigoDLuk4vDhuYDULI2M')
 
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add('Показать')
     msg = bot.reply_to(message, 'Привет, я умею только показывать количество заражённых людей в Казахстане', reply_markup=markup)
     bot.register_next_step_handler(msg, process_step)
 
 def process_step(message):
     chat_id = message.chat.id
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add('Показать')
     if message.text=='Показать':
         page_link = 'https://www.coronavirus2020.kz/ru'
